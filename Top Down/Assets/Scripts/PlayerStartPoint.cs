92294aa996777ef6677fe7350b31fc9617e3cf3b
Earl_Lloyd <mr.minecraft1029384756@gmail.com>
2019-08-13 19:59:48 -0700

    my first commit: here we gooooo!

---
+++ Top Down/Assets/Scripts/PlayerStartPoint.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStartPoint : MonoBehaviour
+{
+
+    private Player_Movement thePlayer;
+    private CameraControl theCamera;
+
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        thePlayer = FindObjectOfType<Player_Movement>();
+        thePlayer.transform.position = new Vector3(transform.position.x, transform.position.y, thePlayer.transform.position.z); 
+        theCamera = FindObjectOfType<CameraControl>();
+        theCamera.transform.position = new Vector3(transform.position.x, transform.position.y, theCamera.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Top Down/Assets/Scripts/ZombieControl.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieControl : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Rigidbody2D myRigidbody2D;
+
+    private bool moving;
+
+    public float timeBetweenMove;
+    private float TimeBetweenMoveCounter;
+    public float TimeToMove;
+    private float TimeToMoveCounter;
+    private Vector3 moveDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+
+        TimeBetweenMoveCounter = timeBetweenMove;
+        TimeToMoveCounter = TimeToMove;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (moving)
+        {
+
+            TimeToMoveCounter -= Time.deltaTime;
+            myRigidbody2D.velocity = moveDirection;
+
+            if (TimeToMoveCounter < 0f)
+            {
+                moving = false;
+                TimeBetweenMoveCounter = timeBetweenMove;
+            }
+        }
+        else
+        {
+            TimeBetweenMoveCounter -= Time.deltaTime;
+            myRigidbody2D.velocity = Vector2.zero;
+
+            if (TimeBetweenMoveCounter < 0f)
+            {
+                moving = true;
+                TimeToMoveCounter = TimeToMove;
+
+                moveDirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+            }
+        }
+    }
+}
