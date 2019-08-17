3a65e243ed84a82fc878fdb0dfe2e22d8c0ad8f9
Earl_Lloyd <mr.minecraft1029384756@gmail.com>
2019-08-17 10:15:50 -0700

    Weapon

---
+++ Top Down/Assets/Scripts/Weapon.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+    public Transform firepoint;
+    private int damage = 10;
+    public GameObject impactEffect;
+    public LineRenderer lineRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            shoot();
+        }
+    }
+
+    void shoot()
+    {
+        RaycastHit2D hitinfo = Physics2D.Raycast(firepoint.position, firepoint.forward);
+
+            if(hitinfo)
+        {
+            ZombieControl enemy = hitinfo.transform.GetComponent<ZombieControl>();
+            if (enemy != null)
+            {
+                //enemy.takeDamage(damage);
+                Debug.Log(hitinfo);
+
+
+                Instantiate(impactEffect, hitinfo.point, Quaternion.identity);
+
+                lineRenderer.SetPosition(0, firepoint.position);
+                lineRenderer.SetPosition(1, hitinfo.point);
+            }
+            else
+            {
+                lineRenderer.SetPosition(0, firepoint.position);
+                lineRenderer.SetPosition(1, firepoint.position + firepoint.forward * 100);
+            }
+        }
+    }
+    void takeDamage()
+    {
+
+    }
+}
